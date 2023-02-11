4d5837e3a8618248f96a56d2e4267945af36d1da
singlelol <singlelol21@gmail.com>
2023-02-11 13:27:17 +0300

    fix: move counter

--- src/components/MoveCounter/MoveCounter.tsx
+++ src/components/MoveCounter/MoveCounter.tsx
@@ -1,23 +1,38 @@
+import './counter.scss';
+/*
 import { Reset } from './Reset';
+
+export const countSteps = (props: string, moveId?: string) => {
+  const moveY = Math.floor(Number(props) / 12);
+  const moveX = Number(props) - moveY * 12;
+  let resultMove = 0;
+  if (props !== moveId) {
+    const nextY = Math.floor(Number(moveId) / 12);
+    resultMove = Math.abs(moveY - nextY);
+    resultMove += Math.abs(moveX - Number(moveId) - nextY * 12);
+  }
+  return resultMove;
+};
+*/
 
 export const MoveCounter = (
   props: string,
-  state: { count?: number; moveId?: string; isString?: boolean },
+  /*
+  moveId?: string,
+  */
+  step: number,
+  count?: number,
 ) => {
-  // const id = props;
-  const { count, moveId, isString } = state;
-  const moveNow = 0;
+  /*
+  const thisId = props;
+  const nextId = moveId;
+  */
+  const moveNow = step;
+  /*
   if (isString) {
     MoveCounter(props, { count: 0, moveId, isString: false });
   }
+  */
 
-  return (
-    <div>
-      {`${moveNow} for ${count}`}
-      <Reset
-{onclick(() => MoveCounter(props, {count: 0, moveId: moveId, isString: true}))}
-
-      />
-    </div>
-  );
+  return <div id='counter'>{`${moveNow} for ${count}`}</div>;
 };
